62ea3e3cadecb20c5c3246dcf2ee7452b30dc267
Carlos Gabriel <cgsalbuquerque@gmail.com>
2021-03-04 22:26:54 -0300

    Middlewares and validations through the routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -13,14 +13,26 @@ const projects = [];
 
 function logRequest(request, response, next) {
   const { method, url } = request;
-  
+
   const logLabel = `[${method.toUpperCase()}] ${url}`;
-  
+
   console.log(logLabel);
-  
+
   return next();
 }
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Invalid project ID." });
+  }
+
+  return next();
+}
+
 app.use(logRequest);
+app.use("/projects/:id", validateProjectId);
 
 app.get("/projects", (request, response) => {
   // Query params: filter and paginate
@@ -83,7 +95,7 @@ app.delete("/projects/:id", (request, response) => {
 
   projects.splice(projectIndex, 1);
 
-  return response.status(204);
+  return response.status(204).json({ message: "Deleted" });
 });
 
 app.listen(3333, () => {
